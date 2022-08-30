5ef395b2d2888380413b8ca53130effb658220ed
Dylan.B <dylan.bernardpro@hotmail.com>
2022-08-30 22:07:09 +0200

    Update card + add new Component checkbox

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+	props: {
+		field: {
+			type: String,
+			required: true
+		},
+		label: {
+			type: String,
+			required: true
+		}
+	},
+}
+</script>
+
+<template>
+<label class="cursor-pointer py-2 flex items-center">
+	<input
+		type="checkbox"
+		@click="$emit('udpateField', field)"
+		class="hidden peer"
+	>
+
+	<div
+		class="min-w-[1rem] min-h-[1rem] bg-white rounded border border-primary flex justify-center items-center	peer-checked:bg-primary peer-checked:text-white text-checkbox"
+	>
+		<i class="fa-solid fa-check hidden"></i>
+	</div>
+
+	<div class="pl-2">{{label}}</div>
+</label>
+</template>
--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -1,5 +1,7 @@
 <script>
+import Checkbox from './Checkbox.vue';
 export default {
+	components: { Checkbox },
 	created() {
 		this.CHAR = 'abcdefghijklmnopqrstuvwxyz';
 		this.SPECIAL_CHAR = '<>[]{}=?()&%$#@!¡?¿*_-.:;,';
@@ -31,20 +33,26 @@ export default {
 			if (this.special) chars += this.SPECIAL_CHAR;
 			if (this.number) chars += this.NUMBER;
 			return chars;
+		},
+		copyPassword()	{
+
+		},
+		updateBooleanField(field) {
+			this[field] = !this[field];
 		}
 	}
 }
 </script>
 
 <template>
-	<div class="h-auto pt-xl w-[19rem] sm:w-[25rem] px-2">
+	<div class="h-auto w-[19rem] sm:w-[25rem] pt-xl px-2">
 		<div class="bg-white rounded-xl drop-shadow-sm">
 
-			<h1 class="text-center py-2 text-xl uppercase font-bold">Password Generator</h1>
+			<h1 class="text-center text-xl uppercase font-bold py-2">Password Generator</h1>
 
 			<div class="px-2 pt-2">
 				<input
-					class="p-2 w-full text-primary border border-primary rounded"
+					class="w-full text-primary border border-primary rounded p-2"
 					type="text"
 					disabled
 					:value="password"
@@ -52,39 +60,30 @@ export default {
 				>
 			</div>
 
-			<div class="px-5 py-4">
+			<div class="px-2 pt-4">
 
 				<div class="py-2">
-					<input type="range" v-model="nbChar" :min="MIN_CHAR" :max="MAX_CHAR">
-					<div>{{nbChar}}</div>
-				</div>
-
-				<div class="py-2">
+					<label>Number of char</label>
 					<input
-						type="checkbox"
-						v-model="caps"
-						id="caps"
-						class="custom-checkbox border border-primary checked:bg-primary"
+						type="number"
+						v-model="nbChar"
+						:min="MIN_CHAR"
+						:max="MAX_CHAR"
+						class="w-full border border-gray-200 focus:outline-primary rounded mt-2 px-2 py-1"
 					>
-					<label class="cursor-pointer pl-2" for="caps">With CAPS</label>
 				</div>
 
-				<div class="py-2">
-					<input type="checkbox" v-model="number" id="number" class="cursor-pointer">
-					<label class="cursor-pointer pl-2" for="number">With Number</label>
-				</div>
+				<checkbox :field="'caps'" :label="'With Caps'" @udpate-field="updateBooleanField"></checkbox>
+				<checkbox :field="'number'" :label="'With Number'" @udpate-field="updateBooleanField"></checkbox>
+				<checkbox :field="'special'" :label="'With Special Char'" @udpate-field="updateBooleanField"></checkbox>
 
-				<div class="py-2">
-					<input type="checkbox" v-model="special" id="special" class="cursor-pointer">
-					<label class="cursor-pointer pl-2" for="special">With Special Char</label>
-				</div>
 			</div>
 
 			<button
 				class="mt-5 py-2 px-4 bg-primary text-white border border-primary rounded w-full hover:bg-white hover:text-primary duration-100 ease-in"
 				@click="generate()"
 				>
-				Générer
+				Generate
 			</button>
 		</div>
 	</div>
